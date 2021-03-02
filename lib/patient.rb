6c4b5d88391edb8103b9b7df69887ee6fd0fe40d
Jouse then <jthen93@gmail.com>
2021-03-02 05:37:04 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,23 @@
+class Patient
+  @@all = []
+  attr_accessor :name, :date, :doctor
+  
+  def initialize(name, date, doctor)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointments.all.select do |app|
+      app.patient == self
+    end
+  end
+  
+  def doctors
+     arr = self.appointments.collect {|app| app.doctor}
+  end
+end
